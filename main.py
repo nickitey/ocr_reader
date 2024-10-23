4076f4df2413407e72344a817d9365ed4496f8d9
nikita.savin <nikita.savin@simbirsoft.com>
2024-10-23 18:39:53 +0300

    feat: добавить вариант с easyocr

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import argparse
-import pytesseract
-import cv2
+import easyocr
+
 
 parser = argparse.ArgumentParser(
     description="EasyOCR image reader. It gets text from image "
@@ -8,17 +8,17 @@ parser = argparse.ArgumentParser(
 )
 parser.add_argument("input_image", type=str, help="Path to image to read")
 parser.add_argument('output_file', type=str, help='Path to result file')
+parser.add_argument('--use-gpu', action=argparse.BooleanOptionalAction, default=True, help='Option, would it use GPU or not. Default is "--use-gpu"')
 arguments = parser.parse_args()
 
 
 def main():
-    image = cv2.imread(arguments.input_image)
-    string = pytesseract.image_to_string(image)
-    with open(arguments.output_file, 'w') as res:
-        res.write(string)
-
-    print(f'Содержимое изображения {arguments.input_image} '
-          f'записано в текстовый файл {arguments.output_file}.')
+	reader = easyocr.Reader(['ru',], gpu=arguments.use_gpu)
+	content = reader.readtext(arguments.input_image, detail=0, paragraph=True)
+	with open(arguments.output_file, 'w') as res:
+		res.write(''.join(content))
+	print(f'Содержимое изображения {arguments.input_image} '
+		  f'записано в текстовый файл {arguments.output_file}.')
 
 	
 if __name__ == "__main__":
